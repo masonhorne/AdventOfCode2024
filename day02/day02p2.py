830376406acbc97079d8a53e0884853c19532f5c
Mason Horne <masonhorne1@gmail.com>
2024-12-02 13:46:24 -0500

    day02 solutions

---
+++ day02/day02p2.py
@@ -0,0 +1,40 @@
+# --- Part Two ---
+# The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+# The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+# Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+# More of the above example's reports are now safe:
+
+# 7 6 4 2 1: Safe without removing any level.
+# 1 2 7 8 9: Unsafe regardless of which level is removed.
+# 9 7 6 2 1: Unsafe regardless of which level is removed.
+# 1 3 2 4 5: Safe by removing the second level, 3.
+# 8 6 4 4 1: Safe by removing the third level, 4.
+# 1 3 6 7 9: Safe without removing any level.
+# Thanks to the Problem Dampener, 4 reports are actually safe!
+
+# Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+
+file = open('day02/day02.in', 'r')
+lines = file.readlines()
+file.close()
+lines = [list(map(int, line.split())) for line in lines]
+
+def valid(line):
+    increasing = line[0] < line[1]
+    for i in range(len(line) - 1):
+        dif = abs(line[i] - line[i + 1])
+        if increasing and line[i] >= line[i + 1]: return False
+        if not increasing and line[i] <= line[i + 1]: return False
+        if dif > 3 or dif < 1: return False
+    return True
+
+def valid2(line):
+    for i in range(len(line)):
+        if valid(line[:i] + line[i + 1:]): return True
+    return False
+
+ans = sum(valid2(line) for line in lines)
+print(ans)
